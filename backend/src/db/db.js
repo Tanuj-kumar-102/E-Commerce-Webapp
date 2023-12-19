9d5dfdebe5b2d9fbe282d682d35a71416b1872e7
Tanuj-kumar-102 <tanujkumarcs102@gmail.com>
2023-12-19 18:36:42 +0530

    add utils and starting controllers

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -4,7 +4,7 @@ const connectionString = process.env.MONGODB_URI;
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect("mongodb+srv://tanujkumarcs102:Tanuj%40123@cluster0.be4xtcf.mongodb.net/Ecommerce");
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`connected to Mongodb at ${conn.connection.host}`);
     } catch (error) {
         console.log(`MongoDB error : ${error}`);
